044f9111a4ac68348e5207ae321269bb13b1c63e
Grupo 2 <gpallero88@gmail.com>
2022-02-27 16:04:19 -0300

    renamed

--- Promesas/Creando-Una-Promesa/creando-promesa.js
+++ Promesas/Creando-Una-Promesa/creando-promesa.js
@@ -1,5 +1,5 @@
 const promise = new Promise((resolve, reject)=>{
-    const number =  5//Math.floor(Math.random*10);
+    const number = Math.floor(Math.random*10);
     setTimeout(() =>number>4?resolve(number):reject(new Error('Menor a 4')), 1000)
 })
 
