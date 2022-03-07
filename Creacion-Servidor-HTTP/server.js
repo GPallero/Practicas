49e3588a9b80c435e7d57266ddb65da34c858332
Grupo 2 <gpallero88@gmail.com>
2022-03-07 19:36:17 -0300

    1er server con HTTP

---
+++ Creacion-Servidor-HTTP/server.js
@@ -0,0 +1,15 @@
+// Pedimos el módulo:
+const http = require('http');
+//En mi constante 'http' quedaron todas las funcionalidades del módulo 'http' (Módulo nativo de Node.js)
+
+//Creamos un server que recibe como parametros un requerimiento y una respuesta.
+const server = http.createServer((request, response) =>{
+    //Definimos una ruta y respondemos.
+    if(request.url === '/'){
+        response.write('Primer server con HTTP')
+        response.end()
+    }
+})
+//Elegimos el puerto
+server.listen(8080)
+console.log('Corriendo en el puerto 8080')
